413e8262e45432b559348aa520217773464f1cf6
Cevino <94840989+cevinov@users.noreply.github.com>
2025-09-03 21:44:01 +0700

    Update func_notif_slack.js

--- uniswapJS_SDK/func_notif_slack.js
+++ uniswapJS_SDK/func_notif_slack.js
@@ -5,7 +5,7 @@
 function sendNotif(exchange, messages) {
   const { IncomingWebhook } = require("@slack/webhook");
   const url =
-    "https://hooks.slack.com/services/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";
+    "https://hooks.slack.com/services/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";
 
   const webhook = new IncomingWebhook(url);
   const data = `ARBITRAGE OPPORTUNITY FOUND for ${exchange}\n ${messages}`;
